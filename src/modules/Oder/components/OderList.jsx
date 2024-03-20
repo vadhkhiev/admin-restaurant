8b75a4a6d55c6a2251500a4639f78b606ac0fd16
Ou SoThon <thongaming25@gmail.com>
2024-03-20 16:42:42 +0700

    add-btn

--- src/modules/Oder/components/OderList.jsx
+++ src/modules/Oder/components/OderList.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./OrderList.css";
-
+import loadingImg from "../../../assets/img/loading.gif";
 function OrderList() {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setError] = useState(false);
+  const [handleCreate] = useState(null);
   const [orders, setOrders] = useState([]);
   const [errorMessage, setErrorMessage] = useState(""); // New state for error message
   const token = localStorage.getItem("token");
@@ -35,40 +36,33 @@ function OrderList() {
     }
   }, [token]);
 
-  const handleStatusChange = async (orderId, newStatus) => {
-    try {
-      await axios.put(
-        `/api/order/status/${orderId}`,
-        { status: newStatus },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      // Update the local state to reflect the change
-      setOrders((prevOrders) =>
-        prevOrders.map((order) =>
-          order.id === orderId ? { ...order, status: newStatus } : order
-        )
-      );
-    } catch (error) {
-      setErrorMessage("Failed to update status. Please try again later.");
-    }
-  };
-
   if (isLoading) {
-    return <h2 className="h1">Loading...</h2>;
+    return (
+      <div className="h1">
+        <p className="fs-4">Loading...</p>
+        <img width={20} src={loadingImg} alt="" />
+      </div>
+    );
   }
 
   if (isError) {
     return <h1>Development Error</h1>;
   }
+  console.log(orders);
 
   return (
     <div className="m-3">
       <h2 className="h1">User Order</h2>
       {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <>
+        <button
+          onClick={handleCreate}
+          style={{ backgroundColor: "#6c738f" }}
+          className="btn text-white fw-bold"
+        >
+          Add
+        </button>
+      </>
       <table
         style={{ color: "#464d69" }}
         className="table-container table bg-white fw-bold"
@@ -76,12 +70,12 @@ function OrderList() {
         <thead>
           <tr>
             <th scope="col">ID</th>
-            <th scope="col">Status</th>
             <th scope="col">Payment</th>
             <th scope="col">Create Date</th>
             <th scope="col">Update Date</th>
             <th scope="col">Table Name</th>
             <td scope="col">Total</td>
+            <th scope="col">Status</th>
             <td scope="col">Action</td>
           </tr>
         </thead>
@@ -89,6 +83,12 @@ function OrderList() {
           {orders.map((order) => (
             <tr key={order.id}>
               <td className="fw-normal">{order.id}</td>
+
+              <td className="fw-normal">{order.paymentMethod}</td>
+              <td className="fw-normal">{order.createdDate}</td>
+              <td className="fw-normal">{order.updateDate}</td>
+              <td className="text-center">{order.tableEntity.name}</td>
+              <td className="fw-normal">{order.totalPrice}</td>
               <td
                 className={`fw-normal ${
                   order.status === "Complete" ? "text-success" : "text-danger"
@@ -96,22 +96,7 @@ function OrderList() {
               >
                 {order.status}
               </td>
-              <td className="fw-normal">{order.paymentMethod}</td>
-              <td className="fw-normal">{order.createdDate}</td>
-              <td className="fw-normal">{order.updateDate}</td>
-              <td className="text-center">{order.tableEntity.name}</td>
-              <td className="fw-normal">{order.totalPrice}</td>
-              <td>
-                <select
-                  className="form-control"
-                  value={order.status}
-                  onChange={(e) => handleStatusChange(order.id, e.target.value)}
-                >
-                  <option value="Complete">Complete</option>
-                  <option value="Cooking">Cooking</option>
-                  <option value="Prepare">Prepare</option>
-                </select>
-              </td>
+              <td></td>
             </tr>
           ))}
         </tbody>
