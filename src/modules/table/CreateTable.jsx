927adeaf4371ebd29585bf1e93eaa41bda5bee61
Him Vannit <vannit9899@gmail.com>
2024-03-21 15:15:52 +0700

    table

--- src/modules/table/CreateTable.jsx
+++ src/modules/table/CreateTable.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import { useState } from 'react'
-import axios from 'axios';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
-const CreateTable = () => {
-    const [values, setValues] = useState({
+const CreateTable = ({ toggleModal }) => {
+  const [values, setValues] = useState({
     id: "",
     name: "",
-    status: "",
+    status: [{}],
     seatCapacity: "",
   });
-  const token = useSelector((state) => state.auth.token) || localStorage.getItem('token');
-  const [modal,setModal] = useState(false);
-  // function toggleModal (){
-  //   setModal(!modal)
-  // }
-
+  const options = [
+    {value:"Booked"},
+    {value:"Available"},
+  ]
+  const token =
+    useSelector((state) => state.auth.token) || localStorage.getItem("token");
 
   const handleCreate = (e) => {
     e.preventDefault();
@@ -23,54 +24,81 @@ const CreateTable = () => {
       .post("/api/table", values, {
         headers: {
           "Content-Type": "application/json",
-          Authorization:
-            `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
         },
       })
       .then((res) => {
         console.log(res);
-        setModal(false)
+        toggleModal();
       })
       .catch((err) => console.log(err));
-    }
+  };
   return (
     <>
-        <div className='d-flex justify-content-center'>
-            <div className='border border-dark w-25 rounded-2 z-1 position-absolute'>
-              <label htmlFor="">Name: </label>
-              <input
-                type="text"
-                name="name"
-                placeholder="Name"
-                onChange={(e) => setValues({ ...values, name: e.target.value })}
-              />
-              <br /> <br />
-              <label htmlFor="">Status: </label>
-              <input
-                type="text"
-                name="status"
-                placeholder="Status"
-                onChange={(e) => setValues({ ...values, status: e.target.value })}
-              />
-              <br /> <br />
-              <label htmlFor="">SeatCapacity: </label>
-              <input
-                type="text"
-                name="seatCapacity"
-                placeholder="SeatCapacity"
-                onChange={(e) => setValues({ ...values, seatCapacity: e.target.value })}
-              />
-              <br /> <br />
-              <div className="d-flex gap-2">
-                <button className="btn btn-primary" onClick={handleCreate}>
-                  Create
-                </button>
-                {/* <button className="btn btn-danger" onClick={toggleModal}>Back</button> */}
-              </div>
-            </div>
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(62,64,87, 0.35)",
+          zIndex: 3,
+        }}
+        className="d-flex justify-content-center align-items-center"
+      >
+        <div
+          className="p-3 border rounded"
+          style={{ width: "30%", backdropFilter: "blur(10px)" }}
+        >
+          <IoCloseCircleOutline
+            style={{ cursor: "pointer" }}
+            onClick={toggleModal}
+            className="fs-3 text-danger mb-3 me-2"
+          />{" "}
+          <br />
+          <div className="">
+            <label className="fs-4 w-25 " htmlFor="">
+              Name:{" "}
+            </label>
+            <input
+              className="ps-2 pe-5 py-2 border-0 rounded-3 w-75"
+              type="text"
+              name="name"
+              placeholder="Name"
+              onChange={(e) => setValues({ ...values, name: e.target.value })}
+            />
+            <br /> <br />
+            <label className="fs-4 w-25 "  htmlFor="">Status:</label>
+            <select className="ps-2 pe-5 py-2 border-0 rounded-3 w-75" name="status" id="" onChange={(e) => setValues({ ...values, status: e.target.value })}>
+              <option value="">Choose Status</option>
+              {options.map((option,index) => (
+                <option value={option.value} key={index}>{option.value}</option>
+              ))}
+            </select>
+            <br /> <br />
+            <label className="fs-4 w-25" htmlFor="">
+              SeatCapacity:{" "}
+            </label>
+            <input
+              className="ps-2 pe-5 py-2 border-0 rounded-3 w-75"
+              type="text"
+              name="seatCapacity"
+              placeholder="SeatCapacity"
+              onChange={(e) =>
+                setValues({ ...values, seatCapacity: e.target.value })
+              }
+            />
+          </div>
+          <div className="d-flex justify-content-center">
+            <button onClick={handleCreate} className="btn btn-primary w-25 mt-4 fw-bold">
+              Create
+            </button>
+          </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default CreateTable;
--- src/modules/table/Table.jsx
+++ src/modules/table/Table.jsx
@@ -8,84 +8,112 @@ import dateTimeFormat from "../Role/core/dateTimeFormat";
 import { useSelector } from "react-redux";
 import CreateTable from "./CreateTable";
 
-
-
 const Table = () => {
   const [tableList, setTableList] = useState([]);
-  const token = useSelector((state) => state.auth.token) || localStorage.getItem('token');
-  const [modal,setModal] = useState(false);
+  const [refresh, setRefresh] = useState(true);
+  const token =
+    useSelector((state) => state.auth.token) || localStorage.getItem("token");
+  const [modal, setModal] = useState(false);
 
-  function toggleModal (){
-    setModal(!modal)
+  function toggleModal() {
+    setModal(!modal);
   }
   useEffect(() => {
-    axios.get(`/api/table`, {
-      headers: {
-        "Content-Type" : "application/json",
-        "Authorization" : `Bearer ${token}`,
-      },
-    })
-    .then(res => {
-      setTableList(res.data.data)
-      console.log(res.data);
-    })
-    .catch(err => console.log(err));
-  }, [])
-
-  // function for delete
-  const handleDelete = (id) =>{
-    const confirm = window.confirm("Are You Sure to delete?");
-    if(confirm){
-      axios.delete(`/api/table/`+ id, {
+    axios
+      .get(`/api/table`, {
         headers: {
-          "Content-Type" : "application/json",
-          "Authorization" : `Bearer ${token}`,
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
       })
-      .then(res => {
-        window.location.reload();
+      .then((res) => {
+        setTableList(res.data.data);
+        setRefresh(!refresh);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
+  }, [refresh]);
+
+  // function for delete
+  const handleDelete = (id) => {
+    const confirm = window.confirm("Are You Sure to delete?");
+    if (confirm) {
+      axios
+        .delete(`/api/table/` + id, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          window.location.reload();
+        })
+        .catch((err) => console.log(err));
     }
-  }
-  
+  };
 
   return (
     <>
-      {modal&&(
-        <CreateTable/>
-      )}
-      <div className="table-container">
-        <table className="table bg-white">
-          <thead>
-            <tr>
-              <th scope="col">Name</th>
-              <th scope="col">Status</th>
-              <th scope="col">SeatCapacity</th>
-              <th scope="col">CreateDate</th>
-              <th scope="col">UpdateDate</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableList.map((tables,index) => (
-              <tr key={index}>
-                <td>{tables.name}</td>
-                <td>{tables.status}</td>
-                <td>{tables.seatCapacity}</td>
-                <td>{dateTimeFormat(tables.createdDate)}</td>
-                <td>{dateTimeFormat(tables.updateDate)}</td>
-                <td className="d-flex gap-2 text-center">
-                  <a type="button" onClick={toggleModal}><IoIosPersonAdd style={{ width: "20px", color: "blue" }} /></a>
-                  <Link to={`/UpdateTable/${tables.id}`}><FaUserEdit style={{ width: "15px", color: "green" }} /></Link>
-                  <a type="button">
-                    <MdDelete onClick={e => handleDelete(tables.id)} style={{ width: "20px", color: "red" }} />
-                  </a>
-                </td>
+      {modal && <CreateTable toggleModal={toggleModal} />}
+      <div>
+        <div className="container mt-3">
+          <div className="row">
+            <div className="col-4 d-flex align-item-center">
+              <span className="fw-bold fs-2 text-dark me-3">Table List</span>
+              <button
+                className="btn btn-primary fs-4 fw-bold px-3"
+                onClick={toggleModal}
+              >
+                Create
+              </button>
+            </div>
+            <div className="col-8 d-flex align-item-center justify-content-end ">
+              <input
+                className="rounded-3 py-2 ps-3 pe-4 mx-2 border-0 fs-4"
+                type="search"
+                placeholder="Search"
+              />
+              <button className="btn btn-outline-secondary fw-bold px-4 fs-4">
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="container mt-3">
+          <table className="table bg-white">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+                <th scope="col">SeatCapacity</th>
+                <th scope="col">CreateDate</th>
+                <th scope="col">UpdateDate</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {tableList.map((tables, index) => (
+                <tr key={index}>
+                  <td>{tables.name}</td>
+                  <td>{tables.status}</td>
+                  <td>{tables.seatCapacity}</td>
+                  <td>{dateTimeFormat(tables.createdDate)}</td>
+                  <td>{dateTimeFormat(tables.updateDate)}</td>
+                  <td className="d-flex gap-2 text-center">
+                    <Link to={`/UpdateTable/${tables.id}`}>
+                      <FaUserEdit style={{ width: "15px", color: "green" }} />
+                    </Link>
+                    <a type="button">
+                      <MdDelete
+                        onClick={(e) => handleDelete(tables.id)}
+                        style={{ width: "20px", color: "red" }}
+                      />
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
