bc67782a86946f3e81a4c5a70c1461889a1b5005
Vivadh Khiev <khievvivadh@gmail.com>
2024-04-08 15:45:25 +0700

    LoL

--- src/modules/Food/Component/ViewFoodCard.jsx
+++ src/modules/Food/Component/ViewFoodCard.jsx
@@ -1,13 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import getFoodById from "../Core/getFoodById";
+import { storeToggleView } from "../Core/allFoodSlice";
 function ViewFoodCard() {
   const token =
     useSelector((state) => state.auth.token) || localStorage.getItem("token");
+  const idEdit = useSelector((state) => state.foodList.idEdit);
+  const [viewFood, setViewFood] = useState({});
+  const dispatch = useDispatch();
+  const getEditingFood = async () => {
+    try {
+      const result = await getFoodById(token, idEdit);
+      return result.data;
+    } catch {}
+  };
 
   useEffect(() => {
-    console.log(token);
-  }, []);
-  return <div>ViewFoodCard</div>;
+    const fetchData = async () => {
+      try {
+        const viewFoodCore = await getEditingFood();
+        setViewFood(viewFoodCore);
+        console.log(viewFood);
+      } catch {}
+    };
+    fetchData();
+  }, [idEdit]);
+  return (
+    <div
+      className="position-absolute top-0 background-view w-50"
+      style={{ marginTop: "250px", marginLeft: "200px" }}
+    >
+      <button
+        onClick={() => {
+          dispatch(storeToggleView(false));
+        }}
+      >
+        ❌
+      </button>
+      <h5>{viewFood.name}</h5>
+    </div>
+  );
 }
 
 export default ViewFoodCard;
