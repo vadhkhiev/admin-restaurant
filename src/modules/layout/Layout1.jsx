ed565a7a71f7eaa7ba67441e2a3cb45a8a2201a9
Vivadh Khiev <khievvivadh@gmail.com>
2024-04-11 07:26:24 +0700

    accept changes

--- src/modules/layout/Layout1.jsx
+++ src/modules/layout/Layout1.jsx
@@ -5,24 +5,10 @@ import Topnav from "./components/Topnav";
 import getroles from "./core/getroles";
 import { useDispatch, useSelector } from "react-redux";
 import { storeRoles } from "./core/roleSlice";
-<<<<<<< HEAD
-
-//
 import { storeFood } from "../Food/Core/slice";
 import getAllFood from "../Food/Core/getAllFood";
 import { storeCategories } from "../Food/Core/allCategoriesSlice";
 import getFoodCategories from "../Food/Core/getFoodCategories";
-import getUsers from "../Usermanangement/core/getUsers";
-import {
-  storeRecentUsers,
-  storeTotalUsers,
-} from "../Usermanangement/core/allusersSlice";
-=======
-import { storeFood} from "../Food/Core/slice";
-import getAllFood from "../Food/Core/getAllFood";
-import { storeCategories } from "../Food/Core/allCategoriesSlice";
-import getFoodCategories from "../Food/Core/getFoodCategories";
->>>>>>> a83ec910c3c2559048a65e0a0e79b213022f6c38
 import axios from "axios";
 import { storeorder } from "../order/core/orderSlice";
 const Layout1 = () => {
@@ -43,22 +29,6 @@ const Layout1 = () => {
     };
     fetchroles();
 
-<<<<<<< HEAD
-    const totalUser = async () => {
-      try {
-        const result = await getUsers();
-        dispatch(storeTotalUsers(result.paging.totals));
-        dispatch(storeRecentUsers(result.data.slice(0, 5)));
-        console.log(result);
-      } catch (error) {
-        console.error("Error in component:", error);
-      }
-    };
-
-    totalUser();
-
-=======
->>>>>>> a83ec910c3c2559048a65e0a0e79b213022f6c38
     const totalOrder = async () => {
       try {
         const response = await axios.get(`/api/orders?page=1`);
@@ -102,7 +72,7 @@ const Layout1 = () => {
 
   return (
     <>
-      <div className="wrapper" style={{background:'#09090b'}}>
+      <div className="wrapper" style={{ background: "#09090b" }}>
         <div className={`sidebar ${open ? "" : "sidebar-hide"}`}>
           <Sidebar toggle={toggle} />
         </div>
@@ -114,7 +84,7 @@ const Layout1 = () => {
           className={`main ${open ? "sidebar-show" : ""}`}
         >
           <Topnav toggle={toggle} />
-          <div  >
+          <div>
             <Outlet />
           </div>
         </div>
