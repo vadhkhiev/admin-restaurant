5636bb8fb4d8cce164a10c4754ae16636a1ba8d7
KakaRith <ahritha@gmail.com>
2024-04-26 15:32:43 +0700

    loading

--- src/modules/Role/components/Access.jsx
+++ src/modules/Role/components/Access.jsx
@@ -1,32 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { GrUserAdmin } from "react-icons/gr";
+import { GrUserAdmin } from 'react-icons/gr';
 import { useSelector } from 'react-redux';
 import useRoles from '../core/action';
 import loadingscreen from '../../../assets/img/loading.gif';
 
 const Access = () => {
   const id = useSelector((state) => state.id.id);
-  const role = useSelector((state) => state.roles.roles)?.find((role)=>role.id === id);
-  const {updateRolePermissions} = useRoles();
+  const role = useSelector((state) => state.roles.roles)?.find((role) => role.id === id);
+  const { updateRolePermissions } = useRoles();
   const [rolePermissions, setPermissions] = useState([]);
-  const [defaultValue, setDefault] = useState([]); 
+  const [defaultValue, setDefault] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const { getRolebyId } = useRoles();
 
   useEffect(() => {
-    getRolebyId(id)
-      .then((res) => {
+    const fetchData = async () => {
+      try {
+        const res = await getRolebyId(id);
         const { permissions } = res.data;
         setPermissions(permissions);
         setDefault(permissions);
-      })
-      .catch((err) => {
-      })
-      .finally(() => {
+      } catch (err) {
+        // Handle error
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+    fetchData();
   }, [id]);
 
   const handleBack = () => {
@@ -37,80 +39,98 @@ const Access = () => {
     setPermissions(defaultValue);
   };
 
-
   const handleSubmit = async () => {
     const data = {
       role_id: id,
       permissions: rolePermissions.map((perm) => ({
         permission_id: perm.id,
-        status : perm.status === 1 ? true : false
-      })), 
+        status: perm.status === 1 ? true : false,
+      })),
     };
-    updateRolePermissions(data);
+    await updateRolePermissions(data);
   };
 
   return (
     <div className='m-3'>
-      {loading ? (
-       <div className='d-flex justify-content-center'>
-          <img width={25} height={25} src={loadingscreen} alt="loading" />
-        </div>
-      ) : (
-        <div className=' py-3 rounded-3 custom-border'  >
-          <div className='p-2 px-3 d-flex justify-content-between '>
+        <div className='py-3 rounded-3 custom-border'>
+          <div className='p-2 px-3 d-flex justify-content-between'>
             <div>
               <span className='p-2 custom-border rounded-3'>
-                <a style={{ textDecoration: 'none' }} className={`p-2 text-white borderbottom`}>Access <GrUserAdmin className='pb-1' /></a>
+                <span style={{ textDecoration: 'none' }} className='p-2 text-white borderbottom'>
+                  Access <GrUserAdmin className='pb-1' />
+                </span>
               </span>
             </div>
             <div>
-              <button onClick={handleBack} className='btn custom-border custom-btn text-white rounded'>{`< Back`}</button>
+              <button onClick={handleBack} className='btn custom-border custom-btn text-white rounded'>
+                {'< Back'}
+              </button>
             </div>
           </div>
-          <h4 className='text-white d-flex align-items-center ms-3 mb-3'>Permission of <span className='text-primary ms-2'>{role.name}</span></h4>
+          <h4 className='text-white d-flex align-items-center ms-3 mb-3'>
+            Permission of{' '}
+            <span className='text-primary ms-2'>{role.name}</span>
+          </h4>
           <main className='rounded-3 custom-border mx-3'>
-          <table className='table table-borderless'>
-  <thead>
-    <tr className='py-3'>
-      <th className='border-bottom border-dark text-white-50'># Permission</th>
-      <th className='border-bottom border-dark text-white-50 d-flex justify-content-end'>check</th>
-    </tr>
-  </thead>
-  <tbody>
-    {rolePermissions?.map((permission, index) => (
-      <tr key={index} className="hover-effect"> 
-        <td className='border-bottom border-dark custom-text text-white-50 fs-5'>{index + 1}{'. '}{permission.name}</td>
-        <td className='border-bottom border-dark custom-text d-flex justify-content-end'>
-          <input 
-            type="checkbox" 
-            className='form-check-input cursor-pointer' 
-            style={{ width: '1.15rem', height: '1.15rem' }} 
-            checked={permission.status === 1} 
-            onChange={(e) => {
-              const newValue = permission.status === 1 ? 0 : 1;
-              const newPermission = { ...permission, status: newValue };
-              const newValueState = rolePermissions.map((p) => (
-                p.id === permission.id ? newPermission : p
-              ));
-              setPermissions(newValueState);
-            }}
-          />
-        </td>
-      </tr>
-    ))}
-  </tbody>
-</table>
+            <table className='table table-borderless'>
+              <thead>
+                <tr className='py-3'>
+                  <th className='border-bottom border-dark text-white-50'>
+                    # Permission
+                  </th>
+                  <th className='border-bottom border-dark text-white-50 d-flex justify-content-end'>
+                    check
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                  {loading ? (
+                    <tr>
+                      <td colSpan="2" className="text-center">
+                        <img width={20} height={20} src={loadingscreen} alt="loading" />
+                      </td>
+                    </tr>
+                  ) : (
+                    rolePermissions?.map((permission, index) => (
+                      <tr key={index} className="hover-effect">
+                        <td className="border-bottom border-dark custom-text text-white-50 fs-5">
+                          {index + 1}. {permission.name}
+                        </td>
+                        <td className="border-bottom border-dark custom-text d-flex justify-content-end">
+                          <input
+                            type="checkbox"
+                            className="form-check-input cursor-pointer"
+                            style={{ width: '1.15rem', height: '1.15rem' }}
+                            checked={permission.status === 1}
+                            onChange={(e) => {
+                              const newValue = permission.status === 1 ? 0 : 1;
+                              const newPermission = { ...permission, status: newValue };
+                              const newValueState = rolePermissions.map((p) =>
+                                p.id === permission.id ? newPermission : p
+                              );
+                              setPermissions(newValueState);
+                            }}
+                          />
+                        </td>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
 
-
+            </table>
           </main>
           <div className='d-flex justify-content-end me-3 mt-3'>
-              <button onClick={handleReset} className='btn btn-danger me-3'>Reset</button>
-              <button onClick={handleSubmit} className='btn custom-border text-white custom-btn'>Save</button>
-            </div>
+            <button onClick={handleReset} className='btn btn-danger me-3'>
+              Reset
+            </button>
+            <button onClick={handleSubmit} className='btn custom-border text-white custom-btn'>
+              Save
+            </button>
+          </div>
         </div>
-      )}
     </div>
   );
 };
 
 export default Access;
+
