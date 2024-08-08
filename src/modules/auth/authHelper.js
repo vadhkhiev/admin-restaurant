8a04073be15b87071d9b28ce7f5412d57249ebfc
KakaRith <ahritha@gmail.com>
2024-08-08 17:07:45 +0700

    handle 401

--- src/modules/auth/authHelper.js
+++ src/modules/auth/authHelper.js
@@ -8,13 +8,29 @@ const removeAuth = () => localStorage.removeItem("token");
 
 
 
- const setUpAxios = () => {
-   axios.defaults.baseURL = "http://13.214.207.172:8001";
+const setUpAxios = () => {
+  axios.defaults.baseURL = "http://13.214.207.172:8001";
+
   axios.interceptors.request.use(config => {
     const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = "Bearer " + localStorage.getItem("token")
+    if (token) {
+      config.headers.Authorization = "Bearer " + token;
+    }
     return config;
   });
-}
+
+  axios.interceptors.response.use(
+    response => response,
+    error => {
+      if (error.response && error.response.status === 401) {
+        removeAuth();
+        if(window.location.pathname !== "/login"){
+           window.location.reload();
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
+};
 
 export { setAuth, removeAuth, getAuth, setUpAxios };
